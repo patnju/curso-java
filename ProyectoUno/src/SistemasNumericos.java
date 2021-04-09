10cc4e83ef957f80f4e93ee734abbd490d08b62c
jptorrealba <jptorrealbat@gmail.com>
2021-04-09 12:30:18 -0400

    Flujo resumido y utilización de control de errores, además usamos JOptionPane con ventanas de dialogo y mensajes

--- ProyectoUno/src/SistemasNumericos.java
+++ ProyectoUno/src/SistemasNumericos.java
@@ -1,14 +1,40 @@
+import javax.swing.*;
+
 public class SistemasNumericos {
     public static void main(String[] args) {
-        int numeroDecimal = 500;
-        System.out.println("numeroDecimal = " + numeroDecimal);
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un número entero");
+        int numeroDecimal = 0;
+        try{
+            numeroDecimal = Integer.parseInt(numeroStr);
+        }catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Debe ingresar un número entero");
+            main(args);
+            System.exit(0);
+        }
 
-        System.out.println("numero binario = " + Integer.toBinaryString(numeroDecimal));
+        String resultadoDecimal = "numeroDecimal = " + numeroDecimal;
+        System.out.println(resultadoDecimal);
 
-        System.out.println("numero Hexadecimal = " + Integer.toHexString(numeroDecimal));
-        System.out.println("numero Octal = " + Integer.toOctalString(numeroDecimal));
+        String resultadoBinario = "numero binario = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(resultadoBinario);
+
+
+
+        String resultadoHexa = "numero Hexadecimal = " + Integer.toHexString(numeroDecimal);
+        System.out.println(resultadoHexa);
+
+        String resultadoOctal = "numero Octal = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(resultadoOctal);
         
         int numeroHex = 0x1f4;
         System.out.println("numeroHex = " + numeroHex);
+
+        int numeroOctal = 036;
+        String mensaje = resultadoDecimal + "\n" +
+                            resultadoBinario + "\n" +
+                            resultadoHexa + "\n" +
+                            resultadoOctal;
+        JOptionPane.showMessageDialog(null, mensaje) ;
+
     }
 }
