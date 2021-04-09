081739d1a3f3e5da634d4013dfc12c6e5abbd6cc
jptorrealba <jptorrealbat@gmail.com>
2021-04-09 11:12:58 -0400

    Corrección de clase caracter

---
+++ ProyectoUno/src/Hola.java
@@ -0,0 +1,15 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola hola");
+        char simbolo = '@';
+        System.out.println("simbolo = " + simbolo);
+
+        char car = '\u0040';
+        System.out.println("car = " + car);
+
+        char decimal = 64;
+        System.out.println("decimal = " + decimal);
+
+    }
+
+}
